76cdc19b867e9a574959cbbd66e9305c7e94b239
hackerstech <34059835+hackerstech@users.noreply.github.com>
2018-04-25 12:28:36 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# NodejsInstaller2018
+Automated Shell Script To Install node js on any Linux 
+Made By hackerstech
+Download google verification bypasser 2018:https://github.com/hackerstech/nodejsinstaller.git
+
+usage :
+1)cd Desktop/
+1
+2)git clone https://github.com/hackerstech/loginbackgroundchangerforgnome.git
+
+3)cd nodejsinstaller
+
+4)chmod +x nodejs.sh 
+
+5)./nodejs.sh
+After running nodejs.sh
+
+1) Installer of Nodejs
+2) Quit
+#? 
+
+
+Thanks for Using you're script
+
---
+++ nodejs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+clear
+echo "###################################################################"
+echo "                                             ▄▄                    "
+echo "                            ██               ▀▀                    "
+echo "  ██▄████▄   ▄████▄    ▄███▄██   ▄████▄    ████     ▄▄█████▄       "
+echo "  ██▀   ██  ██▀  ▀██  ██▀  ▀██  ██▄▄▄▄██     ██     ██▄▄▄▄ ▀       "
+echo "  ██    ██  ██    ██  ██    ██  ██▀▀▀▀▀▀     ██      ▀▀▀▀██▄       "
+echo "  ██    ██  ▀██▄▄██▀  ▀██▄▄███  ▀██▄▄▄▄█     ██     █▄▄▄▄▄██       "
+echo "  ▀▀    ▀▀    ▀▀▀▀      ▀▀▀ ▀▀    ▀▀▀▀▀      ██      ▀▀▀▀▀▀        "
+echo "                                          ████▀                    "
+echo "                                                    nodejsinstaller"
+echo "                                                 by hackerstech2018"  
+echo "###################################################################"
+sleep 2
+
+PS2='Please Enter Your Choice: '
+options=("Installer of Nodejs" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+      "Installer of Nodejs")
+      sudo apt-get update
+      sudo apt-get install wget
+      wget https://nodejs.org/dist/v9.11.1/node-v9.11.1-linux-x64.tar.xz
+      tar -xvf node-v9.11.1-linux-x64.tar.xz
+      cd node-v9.11.1-linux-x64
+      mv -f -u -i bin /usr/
+      mv -f -u -i include /usr/
+      mv -f -u -i share /usr/
+      mv -f -u -i lib /usr/
+      rm -f node-v9.11.1-linux-x64.tar.xz
+      rm -r -f node-v9.11.1-linux-x64
+      node -v
+      npm -v
+      ;;
+     "Quit")
+      break
+      ;;
+   esac
+done
+
+
